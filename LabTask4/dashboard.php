b15d075ea0bf9ca71ea819f6d45b00c77662b808
Md.Shoieb Hossain <shoaibhossain207@gmail.com>
2020-11-03 23:49:19 +0600

    Alhamdulliah Done

---
+++ LabTask4/dashboard.php
@@ -0,0 +1,2 @@
+<p style="background-color:black;width:100%;height:20px;color:White;text-align:center">Welcome to Dashboard
+</p>
---
+++ LabTask4/header.php
@@ -0,0 +1,3 @@
+<p style="background-color:blue;width:100%;height:20px;color:White;text-align:center">
+	Admin
+</p>
